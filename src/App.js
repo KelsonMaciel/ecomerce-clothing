a1c4195d55736a2cc71887d7203d8a8c3dd64761
Kelson Alves Maciel Junior <kelson.junior@linx.com.br>
2020-02-24 14:11:48 -0300

    inicial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch ,Route } from 'react-router-dom';
+import './App.css';
+
+import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
+import SigninAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
+import Header from './components/header/header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SigninAndSignUpPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
